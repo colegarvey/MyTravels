aa1206aa48c9cbb22d5764c49156573d02ea1271
Cole Garvey <colemiester19@outlook.com>
2024-07-24 11:46:29 -0400

    making base file structure

---
+++ client/app/index.js
@@ -0,0 +1,15 @@
+import { Text, View } from 'react-native';
+import { Welcome, Profile } from '../components';
+
+
+const App = () => {
+    return (
+        <View>
+            <Text>APP</Text>
+            <Welcome/>
+            <Profile/>
+        </View>
+    )
+}
+
+export default App;
---
+++ client/components/index.js
@@ -0,0 +1,10 @@
+// Handle imports to shorten and ease component paths
+
+// Home components
+import Welcome from "./home/welcome/Welcome";
+import Profile from "./profile/Profile";
+
+export {
+    Welcome,
+    Profile,
+};
