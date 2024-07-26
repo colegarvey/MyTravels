38be1555164fac8b53cd3ac28f0b3c7ebd74059c
Cole Garvey <colemiester19@outlook.com>
2024-07-25 22:10:15 -0400

    adding application components

---
+++ client/components/auth/login/Login.jsx
@@ -0,0 +1,11 @@
+import { Text } from "react-native";
+
+// import styles from "./login.style";
+
+const Login = () => {
+    return (
+        <Text>Login</Text>
+    )
+}
+
+export default Login;
---
+++ client/components/auth/register/Register.jsx
@@ -0,0 +1,11 @@
+import { Text } from "react-native";
+
+// import styles from "./register.style";
+
+const Register = () => {
+    return (
+        <Text>Register</Text>
+    )
+}
+
+export default Register;
