d8c5f41b5da25cb954d9b4c6133a48468170e9f5
Cole Garvey <colemiester19@outlook.com>
2024-08-07 21:20:28 -0400

    creating register page

--- client/components/auth/register/Register.jsx
+++ client/components/auth/register/Register.jsx
@@ -1,10 +1,43 @@
-import { Text } from "react-native";
+import { useState } from "react";
+import { Alert, Button, Text, TextInput, View } from "react-native";
 
-// import styles from "./register.style";
+import styles from "./register.style";
+
 
 const Register = () => {
+    const [username, setUsername] = useState('');
+    const [key, setKey] = useState('');
+
     return (
-        <Text>Register</Text>
+        <View style={styles.container}>
+            <Text>Register</Text>
+            
+            {/* --USERNAME-- INPUT */}
+            <TextInput style={styles.inField} 
+                onChangeText={setUsername}
+                value={username} 
+
+                placeholder="Username"
+                keyboardType="default"
+            />
+
+            {/* --KEY-- INPUT */}
+            <TextInput style={styles.inField} 
+                onChangeText={setKey}
+                value={key}
+
+                placeholder="Password"
+                keyboardType="default"
+            />
+
+            {/* --SUBMIT-- INPUT */}
+            <Button style={styles.submitBtn}
+                title="Get Started"
+                // onPress={}
+            />
+        </View>
+
+
     )
 }
 
--- client/components/auth/register/register.style.js
+++ client/components/auth/register/register.style.js
@@ -1,7 +1,19 @@
-// import { StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
+import { COLORS, FONT, SIZES } from "../../../constants";
 
-// const styles = StyleSheet.create({
+const styles = StyleSheet.create({
+    container: {
+        marginTop: SIZES.xLarge,
+    },
+    inField: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+    },
+    submitBtn: {
+        backgroundColor: COLORS.accent, 
+    },
+});
 
-// });
-
-// export default styles;
+export default styles;
